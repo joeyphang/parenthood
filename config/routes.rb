480463c9e40962ba6aab5bf9df8392825b20b9f0
joeyphang <joeyphangx@gmail.com>
2018-09-20 14:48:27 +0800

    Create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+	# get 'login', to: 'sessions#new'
+	# get 'logout', to: 'sessions#destroy'
+	# get 'sessions/new'
+	# get 'sessions/create'
+	# get 'sessions/destroy'
+	# resources :users
+
+	get '/register' => 'users#new'
+	post '/users' => 'users#create'
+
+	get '/signin' => 'sessions#new'
+	post '/signin' => 'sessions#create'
+	delete '/signout' => 'sessions#destroy'
+
+	resources :sessions, only: [:create]
+	resources :users, only: [:new, :create, :show, :edit]
+
+	get "/auth/:provider/callback" => "sessions#create_from_omniauth"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
