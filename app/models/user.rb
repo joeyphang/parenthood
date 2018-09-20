91e101234901736c9eab287a2cce2df3ef0393da
joeyphang <joeyphangx@gmail.com>
2018-09-20 16:24:28 +0800

    Add google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,23 @@
 class User < ApplicationRecord
 	has_secure_password
 	validates :email, uniqueness: true, presence: true
+	has_many :authentications, dependent: :destroy
+
+	def self.create_with_auth_and_hash(authentication, auth_hash)
+	   user = self.create!(
+		 first_name: auth_hash["info"]["given_name"],
+		 last_name: auth_hash["info"]["family_name"],
+	     email: auth_hash["info"]["email"],
+	     password: SecureRandom.hex(10)
+	   )
+		user.authentications << authentication
+		return user
+	end
+
+ 	# grab google to access google for user data
+	def google_token
+ 		x = self.authentications.find_by(provider: 'google_oauth2')
+ 		return x.token unless x.nil?
+ 	end
+
 end
